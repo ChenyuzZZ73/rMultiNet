b5513e748f76970b0fa48f6380f7da8ca9bc4794
ChenyuzZZ73 <112918639+ChenyuzZZ73@users.noreply.github.com>
2022-11-11 19:30:20 +0800

    Update function_InitializationLSM.R

--- R/function_InitializationLSM.R
+++ R/function_InitializationLSM.R
@@ -11,7 +11,7 @@
 
 library(rTensor)
 ## Intialization for U,W
-Init_UW <- function(tensor_A, n, r, M, Utrue, Wtrue, pertub = 0.1, int_type) {
+Init_UW <- function(tensor_A, n, r, M, Utrue, Wtrue, perturb = 0.1, int_type) {
   L <- tensor_A@modes[3]
   M1A <- k_unfold(tensor_A, m = 1)@data
   M3A <- k_unfold(tensor_A, m = 3)@data
@@ -30,7 +30,7 @@ Init_UW <- function(tensor_A, n, r, M, Utrue, Wtrue, pertub = 0.1, int_type) {
   if (int_type == "warm") {
     # warm Initialization
     per_U <- matrix(runif(n * n, -0.1, 0.1), ncol = n)
-    per_W <- matrix(runif(L * L, -pertub, pertub), ncol = L)
+    per_W <- matrix(runif(L * L, -perturb, perturb), ncol = L)
     U_int <- svd(Utrue %*% t(Utrue) / n + per_U, nu = r)[["u"]]
     W_int <- svd(Wtrue %*% t(Wtrue) + per_W, nu = M)[["u"]]
     return(list("U_int" = U_int, "W_int" = W_int))
@@ -38,7 +38,7 @@ Init_UW <- function(tensor_A, n, r, M, Utrue, Wtrue, pertub = 0.1, int_type) {
 }
 
 
-InitializationLSM <- function(gen_list, n, r, M, pertub = 0.1, int_type)
+InitializationLSM <- function(gen_list, n, r, M, perturb = 0.1, int_type)
 {
    adj_tensor <- gen_list[[1]]
    theta <- gen_list[[2]]
@@ -48,7 +48,7 @@ InitializationLSM <- function(gen_list, n, r, M, pertub = 0.1, int_type)
    delta_1 <- sqrt(max(rowSums(Utrue_scaled^2)))
    delta_3 <- sqrt(max(rowSums(Wtrue^2)))
    delta <- c(delta_1,delta_1,delta_3)
-   initial_UW <- Init_UW(adj_tensor,n,r,M, Utrue_scaled, Wtrue, pertub, int_type)
+   initial_UW <- Init_UW(adj_tensor,n,r,M, Utrue_scaled, Wtrue, perturb, int_type)
    U_int <- initial_UW$U_int;
    W_int <- initial_UW$W_int;
    return(list(adj_tensor,U_int, W_int, delta))
